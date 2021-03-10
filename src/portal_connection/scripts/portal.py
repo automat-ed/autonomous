cc040a20ab2f85277faa0f91ae48913b24110253
Siliconlad <siliconlad@protonmail.com>
2021-03-10 23:46:07 +0000

    Create ROS node python-socketio hybrid class

--- src/portal_connection/scripts/portal.py
+++ src/portal_connection/scripts/portal.py
@@ -1,29 +1,63 @@
 #!/usr/bin/env python
 
+import rospy
 import socketio
 
-sio = socketio.Client()
 
+class PortalConnection():
+    def __init__(self):
+        rospy.init_node("portal_connection")
 
-@sio.event
-def connect():
-    print('connected')
+        # Initialize state dictionary to hold all information for the Portal
+        self.state = {"battery": 90}
 
+        # Initialize attributes
+        self.sio = socketio.Client()
+        self.connected = False
 
-@sio.event
-def connect_error(e):
-    print(e)
+        # Define ROS parameters
+        self.portal_uri = rospy.get_param("~uri", "http://localhost:2000")
+        self.frequency = rospy.get_param("~frequency", 10)
+        self.secret = rospy.get_param("~secret")
 
+        # Define subscribers
+        # TODO(angus)
 
-@sio.event
-def disconnect():
-    print('disconnected')
+        # Use timer to periodically publish state of this class
+        rospy.Timer(rospy.Duration(1 / self.frequency), self.emit_state)
 
+        # Handle socketio events
+        @self.sio.event
+        def connect():
+            rospy.loginfo("Connected to Portal!")
+            self.connected = True
 
-def main():
-    sio.connect('http://localhost:2000')
-    sio.wait()
+        @self.sio.event
+        def connect_error(e):
+            rospy.logerr(e)
+
+        @self.sio.event
+        def disconnect():
+            rospy.logwarn("Connection to Portal broken.")
+            self.connected = False
+
+        # Connect to Portal
+        self.sio.connect(self.portal_uri, headers={"secret": self.secret})
+
+        # Keep running until ROS is shut down
+        while not rospy.is_shutdown():
+            rospy.spin()
+
+    def emit_state(self, _):
+        if (self.connected):
+            try:
+                self.sio.emit("robot_detail", self.state)
+            except socketio.exceptions.BadNamespaceError:
+                rospy.logwarn("Failed to send data to Portal...will retry in a bit")
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        _ = PortalConnection()
+    except KeyboardInterrupt:
+        pass
