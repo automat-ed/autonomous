9c00c1487df2a265b396d51dab53adcf5db980b8
Siliconlad <siliconlad@protonmail.com>
2021-03-10 18:06:21 +0000

    Fix formatting

--- src/portal_connection/scripts/portal.py
+++ src/portal_connection/scripts/portal.py
@@ -1,24 +1,27 @@
-import asyncio
-import sys
 import socketio
 
 sio = socketio.Client()
+
 
 @sio.event
 def connect():
     print('connected')
 
+
 @sio.event
 def connect_error(e):
     print(e)
+
 
 @sio.event
 def disconnect():
     print('disconnected')
 
+
 def main():
     sio.connect('http://localhost:2000')
     sio.wait()
+
 
 if __name__ == '__main__':
     main()
