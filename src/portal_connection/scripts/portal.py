b7d157a0091c5ec508d99c251187e63d6237b961
Angus Stewart <siliconlad@protonmail.com>
2021-03-27 16:59:33 +0000

    Small updates

--- src/portal_connection/scripts/portal.py
+++ src/portal_connection/scripts/portal.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 
 import rospy
+import json
 import socketio
 from sensor_msgs.msg import NavSatFix
-from automated_msgs.msg import NetworkStatus, NetworkStatusStamped, RobotStateStamped
+from automated_msgs.msg import NetworkStatus, NetworkStatusStamped, RobotState, RobotStateStamped
 
 
 class PortalConnection():
     def __init__(self):
         rospy.init_node("portal_connection")
 
+        # Initialize mapping from RobotState to a string
+        self.state_map = {
+            RobotState.OFF: "Off",
+            RobotState.READY: "Ready",
+            RobotState.DRIVING: "Driving",
+            RobotState.WAITING: "Waiting",
+            RobotState.EMERGENCY: "EMERGENCY",
+            RobotState.BLOCKED: "BLOCKED"
+        }
+
         # Initialize state dictionary to hold all information for the Portal
-        self.state = {"battery": 90}
+        self.state = {
+            "connected": False,
+            "battery": 90,
+            "state": self.state_map[RobotState.OFF],
+            "gps": {"lat": None, "lng": None}
+        }
 
         # Initialize attributes
         self.sio = socketio.Client()
-        self.connected = False
 
         # Define ROS parameters
         self.portal_uri = rospy.get_param("~uri", "http://localhost:2000")
-        self.frequency = rospy.get_param("~frequency", 10)
+        self.frequency = rospy.get_param("~frequency", 1)
         self.secret = rospy.get_param("~secret")
 
         # Define subscribers
@@ -40,21 +55,24 @@ class PortalConnection():
         def connect():
             rospy.loginfo("portal_connection :: Connected to Portal!")
             self.publish_network_status(NetworkStatus.CONNECTED)
-            self.connected = True
+            self.state["connected"] = True
 
         @self.sio.event
         def connect_error(e):
-            self.publish_network_status(NetworkStatus.ERROR)
-            rospy.logerr("portal_connection :: " + e)
+            if isinstance(e, dict):
+                rospy.logerr(json.dumps(e, indent=2))
+            else:
+                self.publish_network_status(NetworkStatus.ERROR)
+                rospy.logerr("portal_connection :: " + e)
 
         @self.sio.event
         def disconnect():
             rospy.logwarn("portal_connection :: Connection to Portal broken.")
             self.publish_network_status(NetworkStatus.DISCONNECTED)
-            self.connected = False
+            self.state["connected"] = False
 
         # Connect to Portal
-        while not self.connected and not rospy.is_shutdown():
+        while not self.state["connected"] and not rospy.is_shutdown():
             try:
                 self.sio.connect(self.portal_uri, headers={"secret": self.secret})
             except socketio.exceptions.ConnectionError:
@@ -64,7 +82,7 @@ class PortalConnection():
         rospy.spin()
 
     def emit_state(self, _):
-        if (self.connected):
+        if (self.state["connected"]):
             try:
                 self.sio.emit("robot_detail", self.state)
             except socketio.exceptions.BadNamespaceError:
@@ -74,7 +92,7 @@ class PortalConnection():
         self.state["gps"] = {"lat": msg.latitude, "lng": msg.longitude}
 
     def robot_state_callback(self, msg):
-        self.state["state"] = {"state": str(msg.state.robot_state)}
+        self.state["state"] = self.state_map[msg.state.robot_state]
 
     def publish_network_status(self, status):
         msg = NetworkStatusStamped()
