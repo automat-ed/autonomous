c250203814a1e7387be12cbfba4f33536a479ce9
Vincenzo <vincenzo.incutti@gmail.com>
2021-03-27 00:58:45 +0000

    compute and plot average error

--- src/localization/scripts/data_viz.py
+++ src/localization/scripts/data_viz.py
@@ -5,6 +5,7 @@ import glob
 import sys
 import re
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
@@ -27,6 +28,9 @@ class DataVisualizer():
         self.gps_global = self.calculate_average_frame(self.gps_global_files)
         self.gps_local = self.calculate_average_frame(self.gps_local_files)
 
+        # Calculate average error between gt and ekf_local / ekf_global
+        self.error_gt_local, self.error_gt_global = self.calculate_average_error()
+
         # Over-write provided options with defaults
         # (see: https://www.stackabuse.com/how-to-merge-two-dictionaries-in-python/)
         self.options = {**{
@@ -39,6 +43,10 @@ class DataVisualizer():
         self.gps_gt.plot(x="x", y="y", ax=axes, label=self.options["names"][0])
         self.gps_global.plot(x="x", y="y", ax=axes, label=self.options["names"][1])
         self.gps_local.plot(x="x", y="y", ax=axes, label=self.options["names"][2])
+
+        # Plot Error
+        plt.gcf().text(0.12, 0.9, "ekf_local error: {}".format(self.error_gt_local), fontsize=11, fontweight="bold")
+        plt.gcf().text(0.62, 0.9,"ekf_global error: {}".format(self.error_gt_global), fontsize=11, fontweight="bold")
 
         # Save figure
         fig_path = self.generate_file_path(os.path.join(self.options["save_path"], "fig.png"))
@@ -86,6 +94,28 @@ class DataVisualizer():
 
         return str(file_path)
 
+    def calculate_average_error(self, ):
+        ''' Compute error as Euclidian distance between pairs of (x, y)'''
+        # Error Ground Truth & EKF Local
+        error_gt_local  = 0
+        # Error Ground Truth & EKF Global
+        error_gt_global = 0
+        # Dataframes have same number of observations, pick any one to iterate with index
+        for index in range(self.gps_gt.shape[0]):
+            point_gt = self.gps_gt.iloc[index, 1:3]
+            point_local = self.gps_local.iloc[index, 1:3]
+            
+            error_local  = np.linalg.norm(point_local-point_gt)
+            error_gt_local += error_local
+            
+            point_global = self.gps_global.iloc[index, 1:3]
+            error_global  = np.linalg.norm(point_global-point_gt)
+            error_gt_global += error_global
+
+        error_gt_local /= self.gps_gt.shape[0]
+        error_gt_global /= self.gps_gt.shape[0]
+
+        return round(error_gt_local, 3), round(error_gt_global, 3)
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
