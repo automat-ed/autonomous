a1d384e597a2a841645032eeade932c7c647e857
Angus Stewart <siliconlad@protonmail.com>
2021-04-13 17:57:25 +0100

    Implement State Machine (#31)

--- src/portal_connection/scripts/portal.py
+++ src/portal_connection/scripts/portal.py
@@ -3,6 +3,7 @@
 import rospy
 import socketio
 from sensor_msgs.msg import NavSatFix
+from automated_msgs.msg import NetworkStatus, NetworkStatusStamped, RobotStateStamped
 
 
 class PortalConnection():
@@ -23,44 +24,63 @@ class PortalConnection():
 
         # Define subscribers
         rospy.Subscriber("/gps/filtered", NavSatFix, self.gps_callback)
+        rospy.Subscriber("/state_machine/state", RobotStateStamped, self.robot_state_callback)
+
+        # Define publishers
+        self.network_state_pub = rospy.Publisher("/state_machine/connection", NetworkStatusStamped, queue_size=1)
 
         # Use timer to periodically publish state of this class
         rospy.Timer(rospy.Duration(1 / self.frequency), self.emit_state)
 
+        # Set reconnect rate
+        self.reconnect_rate = rospy.Rate(0.1)
+
         # Handle socketio events
         @self.sio.event
         def connect():
-            rospy.loginfo("Connected to Portal!")
+            rospy.loginfo("portal_connection :: Connected to Portal!")
+            self.publish_network_status(NetworkStatus.CONNECTED)
             self.connected = True
 
         @self.sio.event
         def connect_error(e):
-            rospy.logerr(e['message'])
+            self.publish_network_status(NetworkStatus.ERROR)
+            rospy.logerr("portal_connection :: " + e)
 
         @self.sio.event
         def disconnect():
-            rospy.logwarn("Connection to Portal broken.")
+            rospy.logwarn("portal_connection :: Connection to Portal broken.")
+            self.publish_network_status(NetworkStatus.DISCONNECTED)
             self.connected = False
 
         # Connect to Portal
-        try:
-            self.sio.connect(self.portal_uri, headers={"secret": self.secret})
-        except socketio.exceptions.ConnectionError:
-            return
+        while not self.connected and not rospy.is_shutdown():
+            try:
+                self.sio.connect(self.portal_uri, headers={"secret": self.secret})
+            except socketio.exceptions.ConnectionError:
+                self.reconnect_rate.sleep()
 
         # Keep running until ROS is shut down
-        while not rospy.is_shutdown():
-            rospy.spin()
+        rospy.spin()
 
     def emit_state(self, _):
         if (self.connected):
             try:
                 self.sio.emit("robot_detail", self.state)
             except socketio.exceptions.BadNamespaceError:
-                rospy.logwarn("Failed to send data to Portal...will retry in a bit")
+                rospy.logwarn("portal_connection :: Failed to send data to Portal...will retry in a bit")
 
     def gps_callback(self, msg):
         self.state["gps"] = {"lat": msg.latitude, "lng": msg.longitude}
+
+    def robot_state_callback(self, msg):
+        self.state["state"] = {"state": str(msg.state.robot_state)}
+
+    def publish_network_status(self, status):
+        msg = NetworkStatusStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.status.status = status
+        self.network_state_pub.publish(msg)
 
 
 if __name__ == '__main__':
